42263dc8bbb2464247f9c1fa7659cdf768330d29
Maxim Kargim <karginmaxx@gmail.com>
2024-11-27 23:18:11 +0300

    first commit

---
+++ src/main/java/com/example/crossroadsAT/CrossroadsAtApplication.java
@@ -0,0 +1,13 @@
+package com.example.crossroadsAT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossroadsAtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossroadsAtApplication.class, args);
+	}
+
+}
